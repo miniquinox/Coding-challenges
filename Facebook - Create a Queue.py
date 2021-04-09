ee629fc24553165252326eab95d45513abac562a
Joaquin Carretero <quinocarreteromartinez@gmail.com>
2021-04-09 10:12:26 -0700

    Create Facebook - Create a Queue.py

---
+++ Facebook - Create a Queue.py
@@ -0,0 +1,17 @@
+queue = []
+ 
+# Adding elements to the queue
+print("Adding to the queue:")
+queue.append('a')
+queue.append('b')
+queue.append('c')
+
+print("Initial queue")
+print(queue)
+ 
+# Removing elements from the queue
+print("\nElements dequeued from queue", queue.pop(0))
+print(queue.pop(0))
+ 
+print("\nQueue after removing elements")
+print(queue)
